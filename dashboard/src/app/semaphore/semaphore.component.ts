f045bfda059293b068a509e2dcafd56639d98ce2
francesca <francesca.venier@tecnicosuperiorekennedy.it>
2020-05-29 17:10:48 +0200

    Define variables

--- dashboard/src/app/semaphore/semaphore.component.ts
+++ dashboard/src/app/semaphore/semaphore.component.ts
@@ -9,7 +9,16 @@ export class SemaphoreComponent implements OnInit {
 
   constructor() { }
 
+  // colore semaforo, gestione dello stato
+
+  n_vehicle = null;
+  humidity = null;
+  temperature = null;
+  atm_pressure = null;
+
   ngOnInit(): void {
+    // ottengo i valori 
+    // assegno i valori necessari
   }
 
 }
