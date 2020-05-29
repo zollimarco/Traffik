ea119c1e1a28d19c873f63c211101894b3bb3d56
MichBass <michelangelo.basso@tecnicosuperiorekennedy.it>
2020-05-29 17:12:52 +0200

    aggiunta cartella models e classe per mappa

---
+++ dashboard/src/app/models/semaphore-map.spec.ts
@@ -0,0 +1,7 @@
+import { SemaphoreMap } from '../models/semaphore-map';
+
+describe('SemaphoreMap', () => {
+  it('should create an instance', () => {
+    expect(new SemaphoreMap()).toBeTruthy();
+  });
+});
---
+++ dashboard/src/app/models/semaphore-map.ts
@@ -0,0 +1,26 @@
+export class SemaphoreMap {
+    coordinates:Coordinates;
+    size: Size;
+    zoom: number;
+    constructor() {
+        this.coordinates = new Coordinates();
+        this.size = new Size()
+        this.zoom = 0;
+    }
+}
+export class Size{
+    width: number;
+    height: number;
+    constructor() {
+        this.width = 0;
+        this.height = 0;
+    }
+}
+export class Coordinates{
+    latitude: number;
+    longitude: number;
+    constructor() {
+        this.latitude = 0;
+        this.longitude = 0;
+    }
+}
