54b167c772c93839134d00fc90ea0831fba0aba9
MichBass <michelangelo.basso@tecnicosuperiorekennedy.it>
2020-05-29 15:43:37 +0200

    cose sulle mappe

--- dashboard/src/app/semaphore-map/semaphore-map.component.ts
+++ dashboard/src/app/semaphore-map/semaphore-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //const configuration = require("config.json");
-import {Api_maps_key} from 'config.json';
+import {api_maps} from 'config/api.json';
 
 @Component({
   selector: 'app-semaphore-map',
@@ -14,7 +14,7 @@ export class SemaphoreMapComponent implements OnInit {
   ngOnInit(): void {
     let latitude = 45.95160;
     let longitude = 12.68054;
-    this.image_url = "https://www.mapquestapi.com/staticmap/v4/getmap?key="+ Api_maps_key +"&size=600,300&type=map&imagetype=jpg&zoom=10&scalebar=false&traffic=false&center="+latitude.toString()+","+longitude.toString()+"&xis=https://s.aolcdn.com/os/mapquest/yogi/icons/poi-active.png,1,c,40.015831,-105.27927&ellipse=fill:0x70ff0000|color:0xff0000|width:2|40.00,-105.25,40.04,-105.30";
+    this.image_url = api_maps.url + api_maps.key +"&size=600,400&type=map&imagetype=jpg&zoom=10&scalebar=false&traffic=false&center="+latitude.toString()+","+longitude.toString()+"&xis=https://s.aolcdn.com/os/mapquest/yogi/icons/poi-active.png,1,c,40.015831,-105.27927&ellipse=fill:0x70ff0000|color:0xff0000|width:2|40.00,-105.25,40.04,-105.30";
   }
 
 }
