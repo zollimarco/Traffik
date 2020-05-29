5e2d2c06b934f8f7764a9f83ad3cb52c5eeefa99
francesca <francesca.venier@tecnicosuperiorekennedy.it>
2020-05-29 12:47:55 +0200

    Add MapQuest Api

--- dashboard/src/app/semaphore-map/semaphore-map.component.ts
+++ dashboard/src/app/semaphore-map/semaphore-map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //const configuration = require("config.json");
-import {Google_maps_key} from 'config.json';
+import {Api_maps_key} from 'config.json';
 
 @Component({
   selector: 'app-semaphore-map',
@@ -12,11 +12,9 @@ export class SemaphoreMapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let latitude = 0;
-    let longitude = 0;
-    console.log(Google_maps_key);
-    this.image_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude.toString() +
-    ","+ longitude.toString() + "&zoom=1&size=100x100&key=" + Google_maps_key;
+    let latitude = 45.95160;
+    let longitude = 12.68054;
+    this.image_url = "https://www.mapquestapi.com/staticmap/v4/getmap?key="+ Api_maps_key +"&size=600,300&type=map&imagetype=jpg&zoom=10&scalebar=false&traffic=false&center="+latitude.toString()+","+longitude.toString()+"&xis=https://s.aolcdn.com/os/mapquest/yogi/icons/poi-active.png,1,c,40.015831,-105.27927&ellipse=fill:0x70ff0000|color:0xff0000|width:2|40.00,-105.25,40.04,-105.30";
   }
 
 }
