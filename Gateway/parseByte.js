2e46d11ae1a537a69f08d57ba4fc15400024602d
Pozza <nicola.pozza@tecnicosuperiorekennedy.it>
2020-06-05 10:08:58 +0200

    creato il json

--- Gateway/parseByte.js
+++ Gateway/parseByte.js
@@ -8,30 +8,35 @@ parser.on('data', parseMsg) // will have 8 bytes per data event
 const gatewayDef = '1111';
 
 function parseMsg(data){
-        console.log(data[1]);
+        //console.log(data[1]);
 
         let msgSize = data.lenght;
 
-	let byte0 = parseInt(data[0],10).toString(2).padStart(8,'0');
-        let byte1 = parseInt(data[1],10).toString(2).padStart(8,'0');
-        let byte2 = parseInt(data[2],10).toString(2).padStart(8,'0');
+        let byte0 = parseInt(data[0],10).toString(2).padStart(8,'0');
+        //let byte1 = parseInt(data[1],10).toString(2).padStart(8,'0');
+        //let byte2 = parseInt(data[2],10).toString(2).padStart(8,'0');
 
 
-	console.log("byte 1 ",byte0); //incrocio
-	console.log("byte 2 ",byte1); //valore
-	console.log("byte 3 ",byte2); //gateway + id sensore
-	console.log("---------------");
+        //console.log("byte 1 ",byte0); //gateway + id sensore
+        //console.log("byte 2 ",byte1); //id incrocio
+        //console.log("byte 3 ",byte2); //valore
+        //console.log("---------------");
 
-	let gateway = byte2.substring(0,4); //prende i primi 4 bit
-        let sensore = byte2.substring(4);  //prende il resto dei bit
+        let gateway = byte0.substring(0,4); //prende i primi 4 bit
+        let sensore = byte0.substring(4);  //prende il resto dei bit
+        var id = data[1];
+        var valore = data[2];
+        let json = {};
 
-	console.log(gateway);
+        if(gateway == '1111'){
 
-	if(gateway == '1111'){
-	
+        json = {
+        "id_incrocio":id,
+        "Semaforo": sensore,
+        "Valore":valore
+        };
 
-	}
+
+        }
+        console.log(json);
 }
-
-
-
