bbdce4395d320f269e9190ea2f44613813a59193
Pozza <nicola.pozza@tecnicosuperiorekennedy.it>
2020-06-22 15:37:07 +0200

    risolto bug

--- Gateway/receive/index.js
+++ Gateway/receive/index.js
@@ -43,19 +43,19 @@ function parseMsg(data) {
 
 	let msgSize = data.lenght;
 
-	let destinatario = 255 //data[0];
-	let mittente = 00 //data[1];
+	let destinatario = data[0];
+	let mittente = data[1];
 
 	let byte3 = parseInt(data[2], 10).toString(2).padStart(8, '0');
-	//let byte4 = parseInt(data[3], 10).toString(2).padStart(8, '0');
-	let sensore = "0010" //byte3.substring(0,4); //prende i primi 4 bit
-	let strada = "0000" //byte3.substring(4);  //prende il resto dei bit
-	let fascia_oraria = "11111111" //byte4.substring(0, 5);
-	let valore1 = "11"; //byte4.substring(5);
+	let byte4 = parseInt(data[3], 10).toString(2).padStart(8, '0');
+	let sensore = byte3.substring(0,4); //prende i primi 4 bit
+	let strada = byte3.substring(4);  //prende il resto dei bit
+	let fascia_oraria = byte4.substring(0, 5);
+	let valore1 = byte4.substring(5);
 	let byte5 = parseInt(data[4], 10).toString(2).padStart(8, '0');
 
-	let valore2 = "11111111";
-	let semaforo_id = 0;
+	let valore2 = byte5;
+	let semaforo_id = mittente;
 	let json = {};
 	let valore = 0;
 	if (destinatario === gatewayDef) {
@@ -94,7 +94,7 @@ function parseMsg(data) {
 			};
 		}
 
-		//console.log(json);
+		console.log(json);
 		client.rpush("dati", JSON.stringify(json));
 
 		client.llen("dati", function (err, data) {
