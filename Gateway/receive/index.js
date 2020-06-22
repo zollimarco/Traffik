ee25bf6fcb1a33baf299f013dbb42bad7ae49bf6
Pozza <nicola.pozza@tecnicosuperiorekennedy.it>
2020-06-22 15:31:40 +0200

    sistemata ricezione dati

--- Gateway/receive/index.js
+++ Gateway/receive/index.js
@@ -35,7 +35,7 @@ let byte3 = '0A';
 //port.write(byte3,'hex');
 
 function roundToTwo(num) {
-    return +(Math.round(num + "e+2")  + "e-2");
+	return +(Math.round(num + "e+2") + "e-2");
 }
 
 function parseMsg(data) {
@@ -49,36 +49,32 @@ function parseMsg(data) {
 	let byte3 = parseInt(data[2], 10).toString(2).padStart(8, '0');
 	//let byte4 = parseInt(data[3], 10).toString(2).padStart(8, '0');
 	let sensore = "0010" //byte3.substring(0,4); //prende i primi 4 bit
-	let strada =  "0000" //byte3.substring(4);  //prende il resto dei bit
-	let fascia_oraria =  "11111111" //byte4.substring(0, 5);
-	let valore1 = "11" //byte4.substring(5);
+	let strada = "0000" //byte3.substring(4);  //prende il resto dei bit
+	let fascia_oraria = "11111111" //byte4.substring(0, 5);
+	let valore1 = "11"; //byte4.substring(5);
 	let byte5 = parseInt(data[4], 10).toString(2).padStart(8, '0');
-	console.log(byte5);
 
-	let valore2 = "‭11111111" //byte5;
+	let valore2 = "11111111";
 	let semaforo_id = 0;
 	let json = {};
 	let valore = 0;
-	//if(destinatario === gatewayDef){
-	var pippo = valore1 + valore2;
-	console.log(pippo, "pippo");
-	valore = parseInt(pippo,2);
-	console.log(valore,"iniziale")
+	if (destinatario === gatewayDef) {
 
-	if (sensore === "0010") {
-		let valoremin = 0;
-		let valoremax = 1023;
-		let A = 0;
-		let B = 100;
+		valore = parseInt(valore1 + valore2, 2);
 
-		valore = ((valore - valoremin) * (B - A))/((valoremax - valoremin) + A);
-		console.log(valore,"2");
-		valore = valore - 40; //per andare sotto zero
-		console.log(roundToTwo(valore));
+		if (sensore === "0010") {
+			let valoremin = 0;
+			let valoremax = 1023;
+			let A = 0;
+			let B = 100;
 
-	}
+			valore = ((valore - valoremin) * (B - A)) / ((valoremax - valoremin) + A);
 
-	if (sensore === "0001") {
+			valore = valore - 40; //per andare sotto zero
+
+		}
+
+		if (sensore === "0001") {
 			json = {
 				"id_incrocio": mittente,
 				"Sensore": sensore,
@@ -87,30 +83,30 @@ function parseMsg(data) {
 				"Valore": valore2
 			};
 
-	}else {
-		json = {
-			"id_incrocio": mittente,
-			"Sensore": sensore,
-			"Strada": strada,
-			"Data": Date(Date.now()),
-			"Fascia_Oraria": fascia_oraria,
-			"Valore": valore
-		};
+		} else {
+			json = {
+				"id_incrocio": mittente,
+				"Sensore": sensore,
+				"Strada": strada,
+				"Data": Date(Date.now()),
+				"Fascia_Oraria": fascia_oraria,
+				"Valore": roundToTwo(valore)
+			};
+		}
+
+		//console.log(json);
+		client.rpush("dati", JSON.stringify(json));
+
+		client.llen("dati", function (err, data) {
+			console.log("Lunghezza della lista: " + data);
+		});
+
+		//elimina l'elemento in coda e restituisce l'elemento eliminato
+		/*	 client.lpop("dati", function(err, data)
+			 {
+					 console.log(data);
+			 });
+		 */
 	}
-
-	//console.log(json);
-/*	client.rpush("dati", JSON.stringify(json));
-
-	client.llen("dati", function (err, data) {
-		console.log("Lunghezza della lista: " + data);
-	});
-*/
-	//elimina l'elemento in coda e restituisce l'elemento eliminato
-	 client.lpop("dati", function(err, data)
-	 {
-			 console.log(data);
-	 });
- 
-//}
-       // console.log(json);
+	// console.log(json);
 }
