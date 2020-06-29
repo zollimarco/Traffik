73a007121c54104d591fb44504bb2ea003efe868
Alex Momex <alexstylehd@gmail.com>
2020-06-29 14:43:23 +0200

    aggiunti web socket

--- Cloud/letturaIotHub/index.js
+++ Cloud/letturaIotHub/index.js
@@ -81,6 +81,38 @@ pool.on('error', function (err) {
 
 var TYPES = require('tedious').TYPES;
 
+const
+  http = require("http"),
+  express = require("express"),
+  socketio = require("socket.io");
+
+const SERVER_PORT = 8080;
+
+// create a new express app
+const app = express();
+// create http server and wrap the express app
+const server = http.createServer(app);
+// bind socket.io to that server
+const io = socketio(server);
+
+let nextVisitorNumber = 1;
+let onlineClients = new Set();
+
+function onNewWebsocketConnection(socket) {
+  console.info(`Socket ${socket.id} has connected.`);
+  onlineClients.add(socket.id);
+
+  socket.on("disconnect", () => {
+    onlineClients.delete(socket.id);
+    console.info(`Socket ${socket.id} has disconnected.`);
+  });
+
+  // echoes on the terminal every "hello" message this socket sends
+  socket.on("hello", helloMsg => console.info(`Socket ${socket.id} says: "${helloMsg}"`));
+
+  // will send a message only to this socket (different than using `io.emit()`, which would broadcast it)
+  socket.emit("welcome", `Welcome! You are visitor number ${nextVisitorNumber++}`);
+}
 
 
 // Display the message content - telemetry and properties.
@@ -93,6 +125,8 @@ var printMessages = function (messages) {
     console.log("Telemetry received: " + new Date(new Date().toUTCString()));
     console.log(JSON.stringify(message.body.Sensore));
     console.log(JSON.stringify(message.body));
+
+    io.emit("Traffik", message.body);  
 
     let id_incrocio = message.body.id_incrocio;
     let Sensore = message.body.Sensore;
@@ -356,4 +390,30 @@ function scriviTraffico(id_inc, id_sem, id_strad, fascia, data, tipo, conteggio)
     connection.execSql(request);
   });
 
-}  
+}
+
+function startServer() {
+
+
+  // example on how to serve a simple API
+  app.get("/random", (req, res) => res.send(generateRandomNumber()));
+
+  // example on how to serve static files from a given folder
+  app.use(express.static("public"));
+
+  // will fire for every new websocket connection
+  io.on("connection", onNewWebsocketConnection);
+
+  // important! must listen from `server`, not `app`, otherwise socket.io won't function correctly
+  server.listen(SERVER_PORT, '0.0.0.0', () => console.info(`Listening on port ${SERVER_PORT}.`));
+
+  // will send one message per second to all its clients
+  let secondsSinceServerStarted = 0;
+  /*setInterval(() => {
+      secondsSinceServerStarted++;
+      io.emit("seconds", secondsSinceServerStarted);
+      io.emit("online", onlineClients.size);
+  }, 1000);*/
+}
+
+startServer();
